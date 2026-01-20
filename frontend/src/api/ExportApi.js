f0738b1c4bf3af8c21bd73f10e2ca900fcf4db36
Yeferson <yefertello2002@gmail.com>
2026-01-20 13:44:32 -0500

    feat: Implementacion de funcion para exportar Clientes a Excel

--- frontend/src/api/ExportApi.js
+++ frontend/src/api/ExportApi.js
@@ -4,3 +4,4 @@ const API_URL = "/exports";
 
 export const exportCotizacionesToExcel = () => fileApi.get(`${API_URL}/cotizacionesToExcel`, { responseType: "blob" });
 export const exportVentasToExcel = () => fileApi.get(`${API_URL}/ventasToExcel`, { responseType: "blob" });
+export const expoetClientesToExcel = () => fileApi.get(`${API_URL}/clientesToExcel`, { responseType: "blob" });
